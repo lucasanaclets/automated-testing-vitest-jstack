3f4e54a29e6fcae24d4f25d48e694efee6b6447c
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-01-28 17:34:40 -0300

    configurando path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
+import path, { dirname } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
